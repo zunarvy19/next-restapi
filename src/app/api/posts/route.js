3410b4084c12a9ef814c321c8f7b3b27e73896a0
zunarvy19 <arvy977#gmail.com>
2024-09-18 22:44:58 +0700

    feat: read data

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -6,16 +6,17 @@ import prisma from "../../../../prisma/client";
 
 export async function GET() {
   // get all of posts
-  const posts = await prisma.post.findMany;
-}
+  const posts = await prisma.post.findMany();
 
-return NextResponse.json(
-  {
-    sucess: true,
-    message: "List Data Posts",
-    data: posts,
-  },
-  {
-    status: 200,
-  }
-);
+  // return response with json
+  return NextResponse.json(
+    {
+      success: true,
+      message: "List Data Posts",
+      data: posts,
+    },
+    {
+      status: 200,
+    }
+  );
+}
