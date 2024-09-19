4e9a9b1d38fecb67bfabe4a8a5f5f656d66cf8f8
zunarvy19 <arvy977#gmail.com>
2024-09-19 18:39:28 +0700

    feat: prisma

--- prisma/migrations/20240919005849_posts/migration.sql
+++ prisma/migrations/20240919005849_posts/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `posts` (
     `title` VARCHAR(191) NOT NULL,
     `content` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
+    `updatedAt` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
