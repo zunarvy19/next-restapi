88c8fa4145f4147cd9ed274f947afeb41596baec
zunarvy19 <arvy977#gmail.com>
2024-09-21 14:12:50 +0700

    feat: update data

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 // import prisma client
 import prisma from "../../../../../prisma/client";
 
+// get data
 export async function GET(request, { params }) {
   // get params id
   const id = parseInt(params.id);
@@ -33,6 +34,39 @@ export async function GET(request, { params }) {
     {
       sucess: true,
       message: "Detail data post",
+      data: post,
+    },
+    {
+      status: 200,
+    }
+  );
+}
+
+// update data
+
+export async function PATCH(request, { params }) {
+  //get id
+  const id = parseInt(params.id);
+
+  // req data
+  const { title, content } = await request.json();
+
+  // update data
+  const post = await prisma.post.update({
+    where: {
+      id,
+    },
+    data: {
+      title: title,
+      content: content,
+      updatedAt: new Date(),
+    },
+  });
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "Data berhasil di update!",
       data: post,
     },
     {
